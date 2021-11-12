584737231bd8478b34c7f6e99ae71aae75b0d043
Pier <pierfrancesco297@gmail.com>
2021-11-12 15:05:38 +0100

    html + array

---
+++ js/script.js
@@ -0,0 +1,30 @@
+//creare un toDo list
+// stampare il listato dei task iniziali
+// far si che ogni task abbia la possibilità di essere cancellato
+// avere la possibilità di aggiungere nuove task
+
+var app = new Vue (
+    {
+        el: '#myapp',
+        data: {
+            newTask: "",
+            tasks: [
+                { text: 'Fare il bucato', done: false },
+                { text: 'andara a fare la spesa', done: false },
+                { text: "andare a correre", done: false },
+                { text: "fare l'esercitazione del pomeriggio", done: false },
+                { text: 'Fare le pulizie', done: false },
+                { text: 'Preparare la cena', done: false }
+                ]
+        },
+        methods: {
+            removeItem(index){
+                this.tasks.splice(index,1);
+            },
+            addTask(){
+                this.tasks.push(this.newTask);
+                this.newTask = "";
+            }
+        }
+    }
+);
